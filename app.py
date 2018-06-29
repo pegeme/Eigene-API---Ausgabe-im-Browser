7be96b197b3a75900896666ddc434b73c22a84a6
Carl Jahn <carl.jahn@telekom.de>
2018-06-29 13:29:18 +0200

    new fixes

--- app.py
+++ app.py
@@ -2,46 +2,42 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 #Wetterdaten fuer Bonn und Darmstadt
-darmstadt =    {
-    "Darmstadt": {
-        "Grad": 22,
-        "Desc": "ohjjeee isch des heisss"
-    }
-}
 
-bonn = {
-    "Bonn": {
-        "Grad": 43,
-        "Desc": "grrrrr isch des kaaalt",
-    }
-}
-
-weather = [{"Wetterdaten": {
-    "Darmstadt": {
-        "Grad": 22,
-        "Desc": "ohjjeee isch des heisss",
-           },
+weather = {
     "Bonn": {
         "Grad": 43,
         "Desc": "grrrrr isch des kaaalt",
     },
-        }
+    "Darmstadt": {
+            "Grad": 22,
+            "Desc": "ohjjeee isch des heisss"
+    }
 }
-]
+
 #AUSGABE BROWSER
-@app.route('/send', methods=['GET','POST'])
-def send():
-    if request.method == 'GET':
-        return render_template('index.html')
-        city = request.form['city']
-        return render_template('city.html', city=city)
-'''
-else:
-     res.status_code == '200'
-        res = requests.get('http://127.0.0.1:5000/send?q=Darmstadt')
-        darmstadt = res.json()
-           print(darmstadt)
-           break
-'''
+@app.route('/index', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/city', methods=['GET','POST'])
+def city():
+    city = request.args.get('q')
+
+    try:
+        content = weather[city]
+    except Exception as e:
+        return render_template('error.html', error="City not found")
+
+    acceptHeader = request.headers.get('Accept')
+
+    if acceptHeader.startswith( 'text/html' ):
+        return render_template('city.html', forecast=content['Desc'], temp=content['Grad'])
+
+    if acceptHeader.startswith( 'application/json' ):
+        return jsonify(content)
+
+    return render_template('city.html', city=city)
+
 if __name__ == "__main__":
-        app.run()
+    app.run(debug=True)
